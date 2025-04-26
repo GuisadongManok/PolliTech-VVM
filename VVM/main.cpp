c1721822a01c335200b85cd99dfe742b15ed4814
8HertzWhenIP <deanachar59@gmail.com>
2025-04-26 15:45:38 +0800

    fixed database issue when deployed

--- VVM/main.cpp
+++ VVM/main.cpp
@@ -5,17 +5,63 @@
 #include <QDebug>
 #include <QSqlError>
 #include <QDir>
+#include <QFile>
+#include <QStandardPaths>
+
+void copyFolder(const QString &sourcePath, const QString &destinationPath)
+{
+    QDir sourceDir(sourcePath);
+    if (!sourceDir.exists())
+        return;
+
+    QDir destDir(destinationPath);
+    if (!destDir.exists())
+        destDir.mkpath(destinationPath);
+
+    QStringList files = sourceDir.entryList(QDir::Files);
+    for (const QString &fileName : files) {
+        QString srcFilePath = sourcePath + "/" + fileName;
+        QString destFilePath = destinationPath + "/" + fileName;
+        if (QFile::copy(srcFilePath, destFilePath)) {
+            QFile::setPermissions(destFilePath, QFile::WriteOwner | QFile::ReadOwner);
+            qDebug() << "Copied file:" << destFilePath;
+        } else {
+            qDebug() << "Failed to copy file:" << srcFilePath;
+        }
+    }
+}
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
 
-    QString dbPath = QCoreApplication::applicationDirPath() + "/database/voting.db";
+    QString dbPath;
+
+#if defined(QT_DEBUG) || defined(QT_RELEASE)
+    // When running inside Qt Creator (Debug or Release)
+    dbPath = QCoreApplication::applicationDirPath() + "/database/voting.db";
+#else
+    // When running as a deployed executable
+    QString writableDir = QStandardPaths::writableLocation(QStandardPaths::AppDataLocation);
+    QString writableDatabaseDir = writableDir + "/database";
+    dbPath = writableDatabaseDir + "/voting.db";
+
+    if (!QFile::exists(dbPath)) {
+        QString bundledDatabaseDir = QCoreApplication::applicationDirPath() + "/database";
+
+        if (QDir(bundledDatabaseDir).exists()) {
+            copyFolder(bundledDatabaseDir, writableDatabaseDir);
+            qDebug() << "Copied database folder to writable location:" << writableDatabaseDir;
+        } else {
+            qDebug() << "Bundled database folder not found:" << bundledDatabaseDir;
+        }
+    }
+#endif
+
     QSqlDatabase db = QSqlDatabase::addDatabase("QSQLITE");
     db.setDatabaseName(dbPath);
 
-    qDebug() << "Looking for DB at:" << QCoreApplication::applicationDirPath() + "/database/voting.db";
-
+    qDebug() << "Opening database at:" << dbPath;
 
     if (!db.open()) {
         qDebug() << "Error: Could not open database!" << db.lastError().text();
@@ -23,7 +69,6 @@ int main(int argc, char *argv[])
 
     loginsystem loginWindow(db);
     loginWindow.show();
-
 
     int result = a.exec();
 
